d3da9219006a72d97e3b7b4c33e07d85ad63d17b
arash <arashparnia@gmail.com>
2016-04-28 09:51:09 +1000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by arash, gray and christopher on 23/04/2016
+ */
+
+import java.sql.SQLException;
+
+public class Main {
+
+        public static void main(String[] args) throws SQLException {
+            // create our actual client and test the database connection
+            UniDbClient uniDB = new UniDbClient();
+
+            if (uniDB.openConnection()) {
+                System.out.println(uniDB.sql("airline_name", "airline"));
+
+
+                uniDB.closeConnection();
+            }
+        }
+
+
+}
---
+++ src/UniDbClient.java
@@ -0,0 +1,114 @@
+/*
+ * Example JDBC client for University Registration DB
+ * Skeleton Java program for COMP9120 Database Application Programming lecture/tutorial
+ * Make sure you have added an appropriate Oracle JDBC driver library
+ */
+
+import java.sql.*;
+
+public class UniDbClient {
+    // connection parameters
+    private final String userid = "apar2844";
+    private final String passwd = "apar2844";
+    private final String database = "oracle12.it.usyd.edu.au:1521:COMP5138";
+
+    // instance variable for the database connection
+    private Connection conn = null;
+
+    /**
+     * class constructor
+     * which loads the Oracle JDBC driver
+     */
+    UniDbClient() {
+        try {
+           /* load Oracle's JDBC driver */
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException no_class_ex) {
+           /* error handling when no JDBC class is found */
+            System.out.println(no_class_ex);
+        }
+    }
+
+    /**
+     * Establishes a connection to the Oracle database.
+     * The connection parameters are read from the instance variables above
+     * (userid, passwd, and database).
+     *
+     * @returns true   on success and then the instance variable 'conn'
+     * holds an open connection to the database.
+     * false  otherwise
+     */
+    public boolean connectToDatabase() {
+        try {
+           /* connect to the database */
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@" + database, userid, passwd);
+           /* If you want to connect to your own database you should remove this line: */
+            // conn.createStatement().execute("ALTER SESSION SET current_schema=COMP5138_DEMO");
+            return true;
+        } catch (SQLException sql_ex) {
+           /* error handling */
+            System.out.println(sql_ex);
+            return false;
+        }
+    }
+
+    /**
+     * open ONE single database connection
+     */
+    public boolean openConnection() {
+        boolean retval = true;
+
+        if (conn != null)
+            System.err.println("You are already connected to Oracle; no second connection is needed!");
+        else {
+            if (connectToDatabase())
+                System.out.println("You successfully connected to Oracle.");
+            else {
+                System.out.println("Oops - something went wrong.");
+                retval = false;
+            }
+        }
+
+        return retval;
+    }
+
+    /**
+     * close the database connection again
+     */
+    public void closeConnection() {
+        if (conn == null)
+            System.err.println("You are not connected to Oracle!");
+        else try {
+            conn.close(); // close the connection again after usage!
+            conn = null;
+        } catch (SQLException sql_ex) {  /* error handling */
+            System.out.println(sql_ex);
+        }
+    }
+
+
+    public boolean sql(String select, String from) throws SQLException {
+        boolean success = false;
+        PreparedStatement stmt = null;
+        String query = "SELECT ? " + "FROM ?";
+        try {
+            stmt = conn.prepareStatement(query);
+            stmt.setString(1, select);
+            stmt.setString(2, from);
+            ResultSet rset = stmt.executeQuery();
+            int nr = 0;
+            while (rset.next()) {
+                success = true;
+                nr++;
+                System.out.println(rset.getString("airline_name"));
+            }
+            if (nr == 0) System.out.println("No entries found.");
+        } catch (SQLException e) {
+            System.out.println("SQLException : " + e);
+        } finally {
+            if (stmt != null) stmt.close();
+        }
+        return success;
+    }
+
+}
